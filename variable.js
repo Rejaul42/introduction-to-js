ce7a1db4c59aa4f7bc573a87a725af47c99157ac
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-28 10:31:22 -0600

    string and boolean type variable added

--- variable.js
+++ variable.js
@@ -1,4 +1,18 @@
+// Numeric variable
 var age = 24;
 var weight = 62;
 var height = 5.6;
 var cgpa = 3.20;
+
+// String type variable
+ 
+var personName = "Reja";
+var address = "Mymensingh, Fulbaria";
+var language = "Bangla";
+var subject = 'CSE';
+
+// Boolean type variable 
+
+var isSingle = false;
+var serious = true;
+var isAngry = true;
