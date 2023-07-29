4be60c169797da05a50d65f7b9e4716d7cb812cd
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 01:03:12 -0600

    operational topic added

---
+++ operation.js
@@ -0,0 +1,25 @@
+var value1 = 30;
+var value2 = 12;
+
+// Additional operation
+var totalValue = value1+value2;
+console.log(totalValue);
+
+// subtraction operation
+var valueDifference = value1 - value2 ;
+console.log(valueDifference);
+
+// Multiplication operation
+var price = 15;
+var quantity = 8;
+var totalPrice = price * quantity;
+console.log(totalPrice);
+
+// Division operation 
+
+var totalNumber = 1120;
+var subject = 13;
+var numberPerSubject = totalNumber / subject;
+var toFixedNumber = numberPerSubject.toFixed(2);
+console.log(toFixedNumber);
+
