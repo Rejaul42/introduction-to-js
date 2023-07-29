439c52d9250803aa9167fe62c3b0d8c8bd59ad1d
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 01:33:25 -0600

    concatenation and conversion added

---
+++ concatenation.js
@@ -0,0 +1,9 @@
+var firstName = 'King';
+var lastName =  'Khan';
+var fullName = firstName + lastName;
+// console.log(fullName);
+
+// concatenation is  the 
+
+var fullNameResult = firstName + ' ' + lastName;
+console.log(fullNameResult);
---
+++ conversion.js
@@ -0,0 +1,9 @@
+var number1 = '50';
+var number2 = 10;
+var number1Int = parseInt(number1);
+var totalNumber = number1Int + number2;
+console.log(totalNumber);
+
+var gpa = '4.30';
+var gpaFloat = parseFloat(gpa);
+console.log(gpaFloat);
