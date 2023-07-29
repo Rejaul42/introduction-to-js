1289ce665b5d1508574faf90c0070a2b433a42bf
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 01:12:25 -0600

    added shorthand

---
+++ shorthand.js
@@ -0,0 +1,21 @@
+var price1 = 31;
+var price2 = 10;
+// Normal way to add a new value to existing variable value 
+price1 = price1 + 10;
+console.log(price1);
+
+// ShortHand: add a new value to the existing variable
+price1 += 10;
+console.log(price1);
+
+// subtraction shorthand
+price2 -= 5;
+console.log(price2);
+
+//Multiplication short hand 
+price1 *= price2;
+console.log(price1);
+
+// Division shorthand technique
+price1 /= price2;
+console.log(price1);
