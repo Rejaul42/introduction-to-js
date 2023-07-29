01e24dc5d7ac3d4f2b44171723c8e46e91a954ec
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 01:58:56 -0600

    added reminder and type of js

---
+++ reminder.js
@@ -0,0 +1,4 @@
+var mangoes = 13;
+var person = 6;
+var mango = mangoes % person;
+console.log(mango);
---
+++ type.js
@@ -0,0 +1,17 @@
+var price = 100;
+// console.log(typeof price);
+var value = '100';
+// console.log(typeof value);
+var isAngry = true;
+// console.log(typeof isAngry);
+var romantic;
+// console.log(typeof romantic);
+
+// toFixed is the return fixed length of value  
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+sum = parseFloat(sum);
+console.log(sum);
