efe45376c17e7a68f0d8505afd895e8c506036f5
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 05:49:59 -0600

    prectices finished

---
+++ prectice1.js
@@ -0,0 +1,4 @@
+var momGives = 1000;
+var appleAndMangoPrice = 700;
+var remainingMoney = momGives - appleAndMangoPrice;
+console.log(remainingMoney);
---
+++ prectice3.js
@@ -0,0 +1,4 @@
+var firstPart = 'I am going to be';
+var secondPart = 'an awesome web developer';
+var fullSentence = firstPart + ' ' + secondPart;
+console.log(fullSentence);
